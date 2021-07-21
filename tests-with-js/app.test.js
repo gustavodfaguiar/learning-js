6b0a7e0f9e0adcaa6a9c0d706b6b58a1afb156a7
Gustavo Aguiar <gustavodfaguiar@gmail.com>
2021-07-20 22:41:25 -0300

    [tests-with-js] Fix tests to user

--- tests-with-js/app.test.js
+++ tests-with-js/app.test.js
@@ -2,7 +2,7 @@ const nock = require('nock')
 const request = require('supertest');
 const server = require('./app');
 
-const { OK, NO_CONTENT, NOT_FOUND } = require('http-status');
+const { OK, NO_CONTENT, NOT_FOUND, UNAUTHORIZED } = require('http-status');
 
 afterAll(() => {
     server.close();
@@ -11,48 +11,40 @@ afterAll(() => {
 describe('User', () => {
 
     test('Given request to user not params should return 404', async () => {
-        nock('http://localhost:3000')
-            .get('/user/')
-            .reply(404);
-
         let error = null;
         let response = null;
         try {
-           response = await request(server).get('/user/');
-        } catch (e) {
-            error = e
+           response = await request(server)
+                                .get('/user/');
+        } catch (err) {
+            error = err;
         }
 
         expect(response.status).toEqual(NOT_FOUND);
     });
 
     test('Given request to not existing user should return 204', async () => {
-        nock('http://localhost:3000')
-            .get('/user/123')
-            .reply(204);
-
         let error = null;
         let response = null;
         try {
-           response = await request(server).get('/user/123');
-        } catch (e) {
-            error = e
+           response = await request(server)
+                        .get('/user/123')
+                        .set('Authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYWRtaW4iLCJpYXQiOjE2MjY3MDQ2Mjh9.EicDDeKpdqsC9ARz7h9fBpFbuzTnz84JQib4QFkf_ZE');
+        } catch (err) {
+            error = err;
         }
-
         expect(response.status).toEqual(NO_CONTENT);
     });
 
     test('Given request to existing user should return 200', async () => {
-        nock('http://localhost:3000')
-            .get('/user/1')
-            .reply(201, {"first_name": "Gustavo", "id": 1, "last_name": "Aguiar"});
-
         let error = null;
         let response = null;
         try {
-           response = await request(server).get('/user/1');
-        } catch (e) {
-            error = e
+            response = await request(server)
+            .get('/user/1')
+            .set('Authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYWRtaW4iLCJpYXQiOjE2MjY3MDQ2Mjh9.EicDDeKpdqsC9ARz7h9fBpFbuzTnz84JQib4QFkf_ZE');
+        } catch (err) {
+            error = err;
         }
 
         expect(response.status).toEqual(OK);
