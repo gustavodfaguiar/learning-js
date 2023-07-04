bb03803eb97e50ba82d4da81728f2625d423f262
Gustavo Aguiar <gustavodfaguiar@gmail.com>
2023-07-04 16:05:29 -0300

    [codeflix-admin] add test to category

--- codeflix-admin/src/category/domain/entities/category.spec.ts
+++ codeflix-admin/src/category/domain/entities/category.spec.ts
@@ -1,8 +1,22 @@
 import { Category } from "./category";
 
-describe("Category Tests", () => {
+describe("Category Unit Tests", () => {
   test("constructor of category", () => {
-    const category = new Category("Movie");
-    expect(category.name).toBe("Movie");
+    const created_at = new Date();
+
+    const props = {
+      name: "Movie",
+      description: "description",
+      is_active: true,
+      created_at
+    };
+
+    const category = new Category(props);
+
+    // expect(category.name).toBe('Movie');
+    // expect(category.description).toBe('description');
+    // expect(category.is_active).toBeTruthy();
+    // expect(category.created_at).toBe(created_at);
+    expect(category.props).toStrictEqual(props);
   });
 });
--- codeflix-admin/src/category/domain/entities/category.ts
+++ codeflix-admin/src/category/domain/entities/category.ts
@@ -1,3 +1,30 @@
+export type CategoryProperties = {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+  created_at?: Date;
+};
+
 export class Category {
-  constructor(public name: string) {}
+  constructor(public readonly props: CategoryProperties) {
+    this.props.description = this.props.description ?? null;
+    this.props.is_active = this.props.is_active ?? true;
+    this.props.created_at = this.props.created_at ?? new Date();
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  get description() {
+    return this.props.description;
+  }
+
+  get is_active() {
+    return this.props.is_active;
+  }
+
+  get created_at() {
+    return this.props.created_at;
+  }
 }
